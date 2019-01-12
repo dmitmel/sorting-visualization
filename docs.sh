43e1b6286f7d43b290b09fb6cf184282320b4be0
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2019-01-12 14:09:42 +0200

    improve docs.sh script

--- docs.sh
+++ docs.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cargo +nightly doc --document-private-items --features doc
+cargo +nightly doc --document-private-items --features doc "$@"
 
 cat > target/doc/index.html <<HTML
 <meta http-equiv="refresh" content="0;url=sorting_visualization/index.html">
