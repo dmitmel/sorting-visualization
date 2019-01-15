f24a8e8f826db76cdcd22fdf946c4b1e5ca6fe62
MichalObi <ndwz123@gmail.com>
2019-01-15 21:06:32 +0100

    Merge sort algo - implementation

--- src/algorithms/merge.rs
+++ src/algorithms/merge.rs
@@ -8,8 +8,8 @@ pub struct MergeSort;
 impl Algorithm for MergeSort {
   fn sort(&self, array: Array) {
       let len = array.len();
-      let mut tmp_array: Vec<u32> = (1..=len as u32).collect();
-      self.split(&array, 0, len as isize - 1, &mut tmp_array);
+      let last = len as usize - 1;
+      self.split(&array, 0, last);
   }
 
   fn name(&self) -> String {
@@ -18,15 +18,67 @@ impl Algorithm for MergeSort {
 }
 
 impl MergeSort {
-    fn split(&self, array: &Array, l_index: isize, r_index: isize, tmp_array: &mut Vec<u32>) {
-        if  r_index - l_index > 1 {
-            let mid = (l_index + r_index) / 2;
-            self.split(array, l_index, mid, tmp_array);
-            self.split(array, mid, r_index, tmp_array);
+    fn split(&self, array: &Array, l: usize, r: usize) {
+        if  l < r {
+            let m: usize = l + (r - l) / 2;
+            self.split(array, l, m);
+            self.split(array, m + 1, r);
+            self.merge(array, l, m, r);
         }
     }
 
-    fn merge() {
+    #[allow(clippy::needless_range_loop)]
+    fn merge(&self, array: &Array, l: usize, m: usize, r: usize) {
+      let mut i:usize = 0;
+      let mut j:usize = 0;
+      let mut k:usize = l;
 
+      let n1:usize = m - l + 1;
+      let n2:usize = r - m;
+
+      let mut l_arr: Vec<u32> = (0..n1 as u32).collect();
+      let mut r_arr: Vec<u32> = (0..n2 as u32).collect();
+
+      for i in 0..n1 {
+          l_arr[i] = array.get(l + i);
+      }
+
+      for j in 0..n2 {
+          r_arr[j] = array.get(m + 1 + j);
+      }
+
+      while i < n1 && j < n2 {
+          if l_arr[i] <= r_arr[j] {
+              array.set_color(k, [0.0, 1.0, 0.0, 0.8]);
+              array.set(k, l_arr[i]);
+              array.wait(5);
+              array.reset_color(k);
+              i += 1;
+          } else {
+              array.set(k, r_arr[j]);
+              array.wait(5);
+              array.reset_color(k);
+              j += 1;
+          }
+          k += 1;
+      }
+
+      while i < n1 {
+          array.set_color(k, [0.0, 1.0, 0.0, 0.8]);
+          array.set(k, l_arr[i]);
+          array.wait(5);
+          array.reset_color(k);
+          i += 1;
+          k += 1;
+      }
+
+      while j < n2 {
+          array.set_color(k, [0.0, 1.0, 0.0, 0.8]);
+          array.set(k, r_arr[j]);
+          array.wait(5);
+          array.reset_color(k);
+          j += 1;
+          k += 1;
+      }
     }
 }
