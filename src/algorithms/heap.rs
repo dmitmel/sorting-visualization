f427210c67fc9d7914f4b8933fae2f6e0be7de0a
MichalObi <ndwz123@gmail.com>
2019-02-03 21:15:57 +0100

    Set main for's for heapify

--- src/algorithms/heap.rs
+++ src/algorithms/heap.rs
@@ -6,7 +6,23 @@
  pub struct HeapSort;
 
   impl Algorithm for HeapSort {
-     fn sort(&self, array: Array) {}
+     fn sort(&self, array: Array) {
+         let len = array.len();
+
+         for i in (len / 2 - 1..0).rev() {
+             heapify(&array, len, i);
+         }
+
+         for i in (len - 1..0).rev() {
+             let temp = array.get(0);
+             array.set(0, i as u32);
+             array.set(i, temp);
+
+             heapify(&array, i, 0);
+         }
+
+         fn heapify(array: &Array, len: usize, i: usize) {}
+     }
 
       fn name(&self) -> String {
        "HeapSort sort".to_string()
