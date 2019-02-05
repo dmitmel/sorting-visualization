824fb288e75e91cab6ec4161c73fe522386e8ea6
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2019-02-05 22:17:59 +0200

    format Heap sort algorithm

--- src/algorithms/heap.rs
+++ src/algorithms/heap.rs
@@ -1,48 +1,48 @@
 //! [Heap sort](https://en.wikipedia.org/wiki/Heapsort)
 
-  use super::{Algorithm, Array};
+use super::{Algorithm, Array};
 
-  /// [Heap sort](https://en.wikipedia.org/wiki/Heapsort
- pub struct HeapSort;
+/// [Heap sort](https://en.wikipedia.org/wiki/Heapsort
+pub struct HeapSort;
 
-  impl Algorithm for HeapSort {
-     fn sort(&self, array: Array) {
-         let first = 0;
-         let n = array.len();
+impl Algorithm for HeapSort {
+  fn sort(&self, array: Array) {
+    let first = 0;
+    let n = array.len();
 
-         for i in (first..n / 2 - 1).rev() {
-             heapify(&array, n, i);
-         }
+    for i in (first..n / 2 - 1).rev() {
+      heapify(&array, n, i);
+    }
 
-         for i in (first..n).rev() {
-             array.swap(first, i);
-             heapify(&array, i, first);
-         }
+    for i in (first..n).rev() {
+      array.swap(first, i);
+      heapify(&array, i, first);
+    }
 
-         fn heapify(array: &Array, n: usize, i: usize) {
-             let mut largest = i;
-             let left = 2 * i + 1;
-             let right = 2 * i + 2;
+    fn heapify(array: &Array, n: usize, i: usize) {
+      let mut largest = i;
+      let left = 2 * i + 1;
+      let right = 2 * i + 2;
 
-             if left < n && array.get(left) > array.get(largest) {
-                 largest = left;
-             }
+      if left < n && array.get(left) > array.get(largest) {
+        largest = left;
+      }
 
-             if right < n && array.get(right) > array.get(largest) {
-                 largest = right;
-             }
+      if right < n && array.get(right) > array.get(largest) {
+        largest = right;
+      }
 
-             if largest != i {
-                 array.set_color(i, [0.0, 1.0, 0.0, 0.8]);
-                 array.swap(i, largest);
-                 heapify(array, n, largest);
-                 array.wait(5);
-                 array.reset_color(i);
-             }
-         }
-     }
+      if largest != i {
+        array.set_color(i, [0.0, 1.0, 0.0, 0.8]);
+        array.swap(i, largest);
+        heapify(array, n, largest);
+        array.wait(5);
+        array.reset_color(i);
+      }
+    }
+  }
 
-      fn name(&self) -> String {
-       "HeapSort sort".to_string()
-     }
- }
+  fn name(&self) -> String {
+    "HeapSort sort".to_string()
+  }
+}
